5a90350be6c8251ee9cc49e0b76c84ab8ac355f4
cdada5454 <2994262185@qq.com>
2025-03-17 02:05:15 +0800

    更新 build.sh

--- buildstart/build.sh
+++ buildstart/build.sh
@@ -1,34 +1,16 @@
 echo "$(date '+%Y-%m-%d %H:%M:%S') - Starting build process..."
 
-
-echo 'src-git lucip https://github.com/xiaorouji/openwrt-passwall' >> /etc/opkg/feeds.conf.default
-echo 'src-git packagep https://github.com/xiaorouji/openwrt-passwall-packages' >> /etc/opkg/feeds.conf.default
-
 PACKAGES=""
-PACKAGES="$PACKAGES luci-app-passwall"
-PACKAGES="$PACKAGES luci-app-passwall_Iptables_Transparent_Proxy"
-PACKAGES="$PACKAGES luci-app-passwall_Nftables_Transparent_Proxy"
-PACKAGES="$PACKAGES luci-app-passwall_INCLUDE_Geoview"
-PACKAGES="$PACKAGES luci-app-passwall_INCLUDE_Haproxy"
-PACKAGES="$PACKAGES luci-app-passwall_INCLUDE_Hysteria"
-PACKAGES="$PACKAGES luci-app-passwall_INCLUDE_NaiveProxy"
-PACKAGES="$PACKAGES luci-app-passwall_INCLUDE_Shadowsocks_Libev_Client"
-PACKAGES="$PACKAGES luci-app-passwall_INCLUDE_Shadowsocks_Libev_Server"
-PACKAGES="$PACKAGES luci-app-passwall_INCLUDE_Shadowsocks_Rust_Client"
-PACKAGES="$PACKAGES luci-app-passwall_INCLUDE_Shadowsocks_Rust_Server"
-PACKAGES="$PACKAGES luci-app-passwall_INCLUDE_ShadowsocksR_Libev_Client"
-PACKAGES="$PACKAGES luci-app-passwall_INCLUDE_ShadowsocksR_Libev_Server"
-PACKAGES="$PACKAGES luci-app-passwall_INCLUDE_Simple_Obfs"
-PACKAGES="$PACKAGES luci-app-passwall_INCLUDE_Trojan_Plus"
-PACKAGES="$PACKAGES luci-app-passwall_INCLUDE_tuic_client"
-PACKAGES="$PACKAGES luci-app-passwall_INCLUDE_V2ray_Geodata"
-PACKAGES="$PACKAGES luci-app-passwall_INCLUDE_V2ray_Plugin"
-PACKAGES="$PACKAGES luci-app-passwall_INCLUDE_Xray"
-PACKAGES="$PACKAGES luci-app-passwall_INCLUDE_Xray_Plugin"
-make image PROFILE=$PROFILE PACKAGES="$PACKAGES" FILES="/home/build/openwrt/files"
+PACKAGES="$PACKAGES -dnsmasq"
+PACKAGES="$PACKAGES dnsmasq-full"
+PACKAGES="$PACKAGES luci"
+PACKAGES="$PACKAGES curl"
+
+make image PACKAGES="$PACKAGES" ROOFS_PARTSIZE="512"
 
 if [ $? -ne 0 ]; then
     echo "$(date '+%Y-%m-%d %H:%M:%S') - Error: Build failed!"
     exit 1
 fi
+
 echo "$(date '+%Y-%m-%d %H:%M:%S') - Build completed successfully."
